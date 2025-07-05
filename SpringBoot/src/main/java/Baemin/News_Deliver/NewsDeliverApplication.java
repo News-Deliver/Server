cc66d54128e68d1ad9a2ffaed0dd9da377359ac0
passionryu <rsy1225@naver.com>
2025-07-05 18:01:38 +0900

    🎉 [프로젝트 시작] : 프로젝트 초기 셋팅
    Spring Boot 프로젝트 초기 세팅- 류성열

---
+++ SpringBoot/src/main/java/Baemin/News_Deliver/NewsDeliverApplication.java
@@ -0,0 +1,13 @@
+package Baemin.News_Deliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsDeliverApplication.class, args);
+	}
+
+}
