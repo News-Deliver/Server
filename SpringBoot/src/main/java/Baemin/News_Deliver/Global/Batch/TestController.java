dea3ded6525e557387919abc1c9dfea8c0d68304
wjkim9 <kin9216@gmail.com>
2025-07-08 20:56:31 +0900

    🔧[구성 변경] : 보안 제거, 테스트 추가, DB 계정 변경
    테스트 용 보안 제거
    테스트 컨트롤러 등록
    root 계정 사용이 USER 불필요 > 제거

---
+++ SpringBoot/src/main/java/Baemin/News_Deliver/Global/Batch/TestController.java
@@ -0,0 +1,13 @@
+package Baemin.News_Deliver.Global.Batch;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String index() {
+        return "hello world";
+    }
+}
