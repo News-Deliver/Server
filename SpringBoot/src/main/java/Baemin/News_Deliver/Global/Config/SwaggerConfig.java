e408f4a136c8dda368dc898c6f7b10218bae857d
passionryu <rsy1225@naver.com>
2025-07-13 00:43:38 +0900

    ✨ [기능 추가] : Swagger UI 추가 + 모니터링 Controller Swagger에 추가 확인
    swagger UI 추가

--- SpringBoot/src/main/java/Baemin/News_Deliver/Global/Config/SwaggerConfig.java
+++ SpringBoot/src/main/java/Baemin/News_Deliver/Global/Config/SwaggerConfig.java
@@ -1,20 +1,28 @@
-//package Baemin.News_Deliver.Global.Config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.info.Info;
-//
-//@Configuration
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        return new OpenAPI()
-//                .info(new Info()
-//                        .version("v1.0.0") // Version
-//                        .title("Lion News Deliver Server")
-//                        .description("Lion News Deliver Server에 대한 Swagger API 문서 페이지"));
-//
-//    }
-//}
+package Baemin.News_Deliver.Global.Config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Swagger springdoc-ui 구성 파일
+ */
+@Configuration
+public class SwaggerConfig {
+
+    /* Swagger 설정 Bean */
+    // 접근 경로 : http://localhost:8080/swagger-ui/index.html
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Lion News Deliver API Document")
+                .version("v0.0.1")
+                .description("배달의 민족 팀 : 멋쟁이 뉴스 배달부의 API 명세서");
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
