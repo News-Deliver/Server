a5082bfb03583630664e512ebcdc4bcd100ff235
passionryu <rsy1225@naver.com>
2025-07-16 09:03:09 +0900

    ✨ [기능 구현] : Sub > 더보기 API 구현 완료

--- SpringBoot/src/main/java/Baemin/News_Deliver/Domain/SubServices/MoreNews/Controller/MoreNewsController.java
+++ SpringBoot/src/main/java/Baemin/News_Deliver/Domain/SubServices/MoreNews/Controller/MoreNewsController.java
@@ -1,18 +1,50 @@
 package Baemin.News_Deliver.Domain.SubServices.MoreNews.Controller;
 
 import Baemin.News_Deliver.Domain.SubServices.MoreNews.Service.MoreNewsService;
+import Baemin.News_Deliver.Global.News.ElasticSearch.dto.NewsEsDocument;
+import Baemin.News_Deliver.Global.ResponseObject.ApiResponseWrapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/sub/more")
+@Tag(name = "Sub/More-News", description = "서브 / 뉴스 더보기 API")
 public class MoreNewsController {
 
     private final MoreNewsService moreNewsService;
 
-    /* 뉴스 더보기 */
-    public void getMoreNews() {
+    /**
+     * 뉴스 더보기 API
+     *
+     * @param historyId 히스토리 고유 번호
+     * @return 10개의 추가적인 뉴스 기사 리스트
+     * @throws IOException 예외 처리
+     */
+    @Operation(summary = "뉴스 더보기 API",description = "해당 History의 고유 번호를 입력 받은 후, 추가적인 검색을 통해 10개의 리스트를 더 반환 ",
+            responses = {
+                    @ApiResponse(responseCode = "201",description = "뉴스 기사가 정상적으로 추가 반환되었습니다."),
+                    @ApiResponse(responseCode = "401",description = "엑세스 토큰 만료"),
+                    @ApiResponse(responseCode = "500",description = "서버 내부 오류 발생")
+            })
+    @GetMapping("/{historyId}")
+    public ResponseEntity<ApiResponseWrapper<List<NewsEsDocument>>> getMoreNews(@PathVariable Long historyId) throws IOException {
 
+        // 뉴스 더보기 서비스 레이어 호출
+        List<NewsEsDocument> newsList = moreNewsService.getMoreNews(historyId);
+
+        return ResponseEntity.status(201)
+                .body(new ApiResponseWrapper<>(newsList,"뉴스 기사가 정상적으로 추가 반환되었습니다."));
 
     }
 }
--- SpringBoot/src/main/java/Baemin/News_Deliver/Domain/SubServices/MoreNews/Service/MoreNewsService.java
+++ SpringBoot/src/main/java/Baemin/News_Deliver/Domain/SubServices/MoreNews/Service/MoreNewsService.java
@@ -5,13 +5,24 @@ import Baemin.News_Deliver.Domain.Kakao.repository.HistoryRepository;
 import Baemin.News_Deliver.Domain.SubServices.Exception.SubServicesException;
 import Baemin.News_Deliver.Domain.SubServices.MoreNews.DTO.MoreNewsResponse;
 import Baemin.News_Deliver.Global.Exception.ErrorCode;
+import Baemin.News_Deliver.Global.News.ElasticSearch.dto.NewsEsDocument;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch._types.query_dsl.TextQueryType;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.Hit;
+import co.elastic.clients.json.JsonData;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -19,23 +30,118 @@ import java.util.Optional;
 public class MoreNewsService {
 
     private final HistoryRepository historyRepository;
+    private final ElasticsearchClient client;
 
-    /* 뉴스 추가 검색 메서드 */
-    public List<MoreNewsResponse> getMoreNews(Long historyId) {
+    /**
+     * 뉴스 추가 검색 메서드
+     *
+     * @param historyId 히스토리 고유 번호
+     * @return 15개의 뉴스 기사 리스트
+     * @throws IOException IO 예외
+     */
+    public List<NewsEsDocument> getMoreNews(Long historyId) throws IOException {
 
         /* 히스토리 객체 반환 */
         History history = historyRepository.findById(historyId)
                 .orElseThrow(() -> new SubServicesException(ErrorCode.HISTORY_NOT_FOUND));
+        log.info("히스토리 객체 반환 성공 : {}", history);
 
         /* 히스토리 세부 정보 반환 */
         String settingKeyword = history.getSettingKeyword(); // 설정 키워드 반환
         String blockKeyword = history.getBlockKeyword(); // 제외 키워드
         LocalDateTime publishedAt = history.getPublishedAt(); // 날짜
+        log.info("히스토리 세부 정보 반환 (settingKeyword,publishedAt, blockKeyword) : {},{},{}", settingKeyword,publishedAt, blockKeyword);
 
+        // settingKeyword 문자열을 리스트로 변환
+        List<String> settingKeywords = Arrays.stream(settingKeyword.split(","))
+                .map(String::trim)        // 공백 제거
+                .filter(s -> !s.isEmpty()) // 빈 문자열 제거
+                .toList();
+        log.info("설정 리스트 반환 : {}", settingKeywords);
 
+        // blockKeyword 문자열을 리스트로 변환
+        List<String> blockKeywords = Arrays.stream(blockKeyword.split(","))
+                .map(String::trim)
+                .filter(s -> !s.isEmpty())
+                .toList();
+        log.info("제외 리스트 반환 : {}", blockKeywords);
 
-        return null;
+        /* Elastic Search 검색 엔진 호출 : List 반환 */
+
+        // 포함 키워드 필터링
+        // 포함 키워드 쿼리
+        Query includeKeywordQuery = Query.of(q -> q
+                .bool(b -> b
+                        .should(settingKeywords.stream()
+                                .map(kw -> Query.of(q2 -> q2
+                                        .multiMatch(m -> m
+                                                .query(kw)
+                                                .fields("title^3", "summary^2")
+                                                .type(TextQueryType.BoolPrefix)
+                                        )
+                                ))
+                                .collect(Collectors.toList())
+                        )
+                        .minimumShouldMatch("1")
+                )
+        );
+
+        // 제외 키워드 쿼리
+        Query excludeKeywordQuery = Query.of(q -> q
+                .bool(b -> b
+                        .should(blockKeywords.stream()
+                                .map(kw -> Query.of(q2 -> q2
+                                        .multiMatch(m -> m
+                                                .query(kw)
+                                                .fields("title^3", "summary^2")
+                                                .type(TextQueryType.BoolPrefix)
+                                        )
+                                ))
+                                .collect(Collectors.toList())
+                        )
+                )
+        );
+
+        // 날짜 필터링
+        Query dateFilter = Query.of(q -> q
+                .range(r -> r
+                        .field("published_at") // 메시지 전송일
+                        .gte(JsonData.of(publishedAt))
+                )
+        );
+
+        // 전체 쿼리 (키워드와 블랙 키워드를 포함한 전체 쿼리)
+        Query finalQuery = Query.of(q -> q
+                .bool(b -> b
+                        .must(includeKeywordQuery)
+                        .must(dateFilter)
+                        .mustNot(excludeKeywordQuery)
+                )
+        );
+
+        // 검색 엔진
+        SearchRequest request = SearchRequest.of(s -> s
+                .index("news-index-nori")
+                .query(finalQuery)
+                .size(15)
+                .sort(sort -> sort
+                        .score(sc -> sc.order(co.elastic.clients.elasticsearch._types.SortOrder.Desc))
+                )
+        );
+        log.info("검색 엔진 결과  : {}", request);
+
+        SearchResponse<NewsEsDocument> response = client.search(request, NewsEsDocument.class);
+        log.info("SearchResponse<NewsEsDocument> response : {}", response);
+
+        response.hits().hits().forEach(hit -> {
+            assert hit.source() != null;
+            log.info(" {} | score: {}", hit.source().getTitle(), hit.score());
+        });
+
+        // 그대로 ES 문서 리스트 반환
+        return response.hits().hits().stream()
+                .map(Hit::source)
+                .toList();
     }
-
 
 }
